8a2932addf77429ca71615dffc8ed8ac58ad31a8
Александр Чернобров <alesandro83@mail.ru>
2023-05-20 16:42:54 +0300

    Продолжаю делать меню

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
-const menuButton = document.querySelector('.header__toggle--open');
+const menuButton = document.querySelector('.header__toggle');
 const nav = document.querySelector('.nav');
 
 const navItemMaterials = document.querySelector('.nav__item-materials');
 const navMaterials = document.querySelector('.nav__materials');
+const navButtonBack = document.querySelector('.nav__button-back');
+const navMaterialsItem = document.querySelectorAll('.nav__materials-item');
 
-const headerToggleBack = document.querySelector('.header__toggle-back');
+//const headerToggleBack = document.querySelector('.header__toggle-back');
 
 const buttons = document.querySelectorAll('.button');
 const popup = document.querySelector('.popup');
@@ -12,34 +14,73 @@ const popupQuit = document.querySelector('.popup__quit');
 
 const backgroundOverlay = document.querySelector('.background-overlay');
 
-function onNavItemMaterials() {
-  navMaterials.style.display = 'block';
-  headerToggleBack.style.display = 'block';
-};
-
 menuButton.addEventListener ('click', function (evt) {
   evt.preventDefault();
-  if (menuButton.classList.contains('header__toggle--open')) {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
     menuButton.classList.remove('header__toggle--open');
     menuButton.classList.add('header__toggle--close');
-    nav.style.display = 'block';
-    navItemMaterials.addEventListener('click',  onNavItemMaterials);
     //document.body.style.overflow = "hidden";
   } else {
-    menuButton.classList.remove('header__toggle--close');
+    nav.classList.add('nav--closed');
     menuButton.classList.add('header__toggle--open');
-    nav.style.display = 'none';
-    headerToggleBack.style.display = 'none';
-    navMaterials.style.display = 'none';
-    navItemMaterials.removeEventListener('click',  onNavItemMaterials);
+    menuButton.classList.remove('header__toggle--close');
+
+    // headerToggleBack.style.display = 'none';
+    //navMaterials.style.display = 'none';
+    // navItemMaterials.addEventListener('click',  () => {
+    //   navMaterials.classList.add('nav__materials--closed')
+    // });
     //document.body.style.overflow = "visible";
   }
 });
 
-headerToggleBack.addEventListener('click', () => {
-  navMaterials.style.display = 'none';
-  headerToggleBack.style.display = 'none';
+navItemMaterials.addEventListener('click',  () => {
+  navMaterials.classList.remove('nav__materials--closed');
+  // navMaterials.style.display = 'block';
 });
+
+menuButton.addEventListener('click', () => {
+  navMaterials.classList.add('nav__materials--closed');
+  //navMaterials.style.display = 'none';
+});
+
+navButtonBack.addEventListener('click', () => {
+  navMaterials.classList.add('nav__materials--closed');
+  // navMaterials.style.display = 'none';
+});
+
+navMaterialsItem.addEventListener('click', () => {
+  nav.style.display = 'none'
+})
+
+// function onNavItemMaterials() {
+//   navMaterials.classList.remove('nav__materials--closed');
+// };
+
+// menuButton.addEventListener ('click', function (evt) {
+//   evt.preventDefault();
+//   if (menuButton.classList.contains('header__toggle--open')) {
+//     menuButton.classList.remove('header__toggle--open');
+//     menuButton.classList.add('header__toggle--close');
+//     nav.style.display = 'block';
+//     navItemMaterials.addEventListener('click',  onNavItemMaterials);
+//     //document.body.style.overflow = "hidden";
+//   } else {
+//     menuButton.classList.remove('header__toggle--close');
+//     menuButton.classList.add('header__toggle--open');
+//     nav.style.display = 'none';
+//     headerToggleBack.style.display = 'none';
+//     navMaterials.style.display = 'none';
+//     navItemMaterials.removeEventListener('click',  onNavItemMaterials);
+//     //document.body.style.overflow = "visible";
+//   }
+// });
+
+// headerToggleBack.addEventListener('click', () => {
+//   navMaterials.style.display = 'none';
+//   headerToggleBack.style.display = 'none';
+// });
 
 buttons.forEach(function(btn) {
   btn.addEventListener('click', function(e) {
